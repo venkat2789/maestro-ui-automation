c60d5713f9d7d39e4de71189685ade2d98cefd46
Venkat S <94206129+venkat2789@users.noreply.github.com>
2023-02-03 20:06:20 -0500

    Update scipts-for-ga.sh

--- scipts-for-ga.sh
+++ scipts-for-ga.sh
@@ -1,2 +1,14 @@
-#list-avds
+echo printing work directory
+pwd
+
+echo listing files
+ls
+
+echo listing avds
 /Users/runner/Library/Android/sdk/emulator/emulator -list-avds
+
+echo installing app
+/Users/runner/Library/Android/sdk/platform-tools/adb install /Users/runner/work/maestro-ui-automation/maestro-ui-automation/WikipediaSample.apk
+
+echo get package names
+/Users/runner/Library/Android/sdk/platform-tools/adb shell pm list packages -3 -f
