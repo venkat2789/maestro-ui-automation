b2ae8492715d941aa7842590b515a4071c4ef36e
Venkat S <94206129+venkat2789@users.noreply.github.com>
2023-02-04 15:31:09 -0500

    Move maestro install step into sh file

--- scipts-for-ga.sh
+++ scipts-for-ga.sh
@@ -1,17 +1,22 @@
-echo >> printing work directory
+echo >>printing work directory
 pwd
 
-echo >> listing project files
+echo >>listing project files
 ls
 
-echo >> listing avds
+echo >>listing avds
 /Users/runner/Library/Android/sdk/emulator/emulator -list-avds
 
-echo >> installing app
+echo >>installing app
 /Users/runner/Library/Android/sdk/platform-tools/adb install /Users/runner/work/maestro-ui-automation/maestro-ui-automation/WikipediaSample.apk
 
-echo >> getting package names
+echo >>getting package names
 /Users/runner/Library/Android/sdk/platform-tools/adb shell pm list packages -3 -f
 
-echo >> running tests
+echo >>installing maestro
+curl -Ls "https://get.maestro.mobile.dev" | bash
+export PATH="$PATH":"$HOME/.maestro/bin"
+maestro --version
+
+echo >>running tests
 maestro test ./folderWithTests/wiki-generalFlow.yaml
