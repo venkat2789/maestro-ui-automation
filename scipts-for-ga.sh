a814dc24a69d525ecce1d971501d6d9428106037
Venkat S <94206129+venkat2789@users.noreply.github.com>
2023-02-04 16:07:22 -0500

    Add junit test output

--- scipts-for-ga.sh
+++ scipts-for-ga.sh
@@ -1,22 +1,24 @@
-echo >>printing work directory
+#!/bin/bash
+
+echo ">>printing work directory"
 pwd
 
-echo >>listing project files
+echo ">>listing project files"
 ls
 
-echo >>listing avds
+echo ">>listing avds"
 /Users/runner/Library/Android/sdk/emulator/emulator -list-avds
 
-echo >>installing app
+echo ">>installing app"
 /Users/runner/Library/Android/sdk/platform-tools/adb install /Users/runner/work/maestro-ui-automation/maestro-ui-automation/WikipediaSample.apk
 
-echo >>getting package names
+echo ">>getting package names"
 /Users/runner/Library/Android/sdk/platform-tools/adb shell pm list packages -3 -f
 
-echo >>installing maestro
+echo ">>installing maestro"
 curl -Ls "https://get.maestro.mobile.dev" | bash
 export PATH="$PATH":"$HOME/.maestro/bin"
 maestro --version
 
-echo >>running tests
-maestro test ./folderWithTests/wiki-generalFlow.yaml
+echo ">>running tests"
+maestro test ./folderWithTests/wiki-generalFlow.yaml --format junit
